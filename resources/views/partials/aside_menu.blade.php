77a05346255799ba7828635d7ebc64c1612172e8
xJustFox <dodydaversa@gmail.com>
2024-02-28 13:33:44 +0100

    [Update] Small aesthetic changes

--- resources/views/partials/aside_menu.blade.php
+++ resources/views/partials/aside_menu.blade.php
@@ -2,20 +2,20 @@
     <ul class=" list-unstyled ">
         <li>
             <a class="asideMenuLink" href="{{ route('admin.dashboard') }}">
-                <i class="fa-solid fa-gauge fa-lg" style="color: #FFf;"></i>
-                {{ __('Dashboard') }}
+                <i class="fa-solid fa-gauge fa-lg {{ Route::currentRouteName() == 'admin.dashboard' ? 'activeRoute' : '' }}"></i>
+                <span class="{{ Route::currentRouteName() == 'admin.dashboard' ? 'activeRoute' : '' }}">{{ __('Dashboard') }}</span>
             </a>
         </li>
         <li>
             <a class="asideMenuLink" href="{{ route('admin.projects.index')}}">
-                <i class="fa-solid fa-folder-tree fa-lg" style="color: #fff;"></i>
-                {{ __('Projects') }}
+                <i class="fa-solid fa-folder-tree fa-lg {{ Route::currentRouteName() == 'admin.projects.index' ? 'activeRoute' : '' }}"></i>
+                <span class="{{ Route::currentRouteName() == 'admin.projects.index' ? 'activeRoute' : '' }}">{{ __('Projects') }}</span>
             </a>
         </li>
         <li>
             <a class="asideMenuLink" href="{{ route('admin.types.index')}}">
-                <i class="fa-solid fa-cubes fa-lg" style="color: #fff;"></i>
-                {{ __('Types') }}
+                <i class="fa-solid fa-cubes fa-lg {{ Route::currentRouteName() == 'admin.types.index' ? 'activeRoute' : '' }}"></i>
+                <span class="{{ Route::currentRouteName() == 'admin.types.index' ? 'activeRoute' : '' }}">{{ __('Types') }}</span>
             </a>
         </li>
     </ul>
